3221d67d620a951f404e6c963e10370f36c94617
ashikkhan34 <ashikkhan314167@gmail.com>
2024-11-22 21:06:53 +0600

    assignment-9

--- src/components/onsell/OnSell.jsx
+++ src/components/onsell/OnSell.jsx
@@ -19,7 +19,7 @@ const OnSell = () => {
             });
     }, []); 
     return (
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3' data-aos="flip-right">
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3' data-aos="fade-right">
             {coupons.filter((sell)=> sell.isSaleOn).map((coupon) => <OnSellCard coupon={coupon}></OnSellCard>
                     )}
         </div>
